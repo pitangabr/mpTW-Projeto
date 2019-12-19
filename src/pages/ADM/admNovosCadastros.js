b02f18d362bbe81d4cbf56ba9bcc978d9df0e877
pitangabr <rocha04santos@gmail.com>
2019-12-19 17:19:50 -0200

    Projet_TWmarketPlace

--- src/pages/ADM/admNovosCadastros.js
+++ src/pages/ADM/admNovosCadastros.js
@@ -3,19 +3,9 @@ import '../../assets/CSS/ADM/PerfilUsuario.css';
 import MenuAdm from '../../Componentes/MenuADM';
 import { Link } from 'react-router-dom';
 
-import { MDBBtn, MDBCollapse } from "mdbreact";
+import imaX from '../../assets/imagens/imaX.png';
 
 class admNovosCadastros extends Component {
-    state = {
-        collapseID: ""
-      }
-      
-      toggleCollapse = collapseID => () => {
-        this.setState(prevState => ({
-          collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-        }));
-      }
-    
     render() {
         return(
         <>
@@ -59,34 +49,6 @@ class admNovosCadastros extends Component {
                     </div>
                 </section>
             </main>
-
-            <MDBBtn
-        color="primary"
-        href="!#"
-        onClick={this.toggleCollapse("basicCollapse")}
-        style={{ marginBottom: "1rem" }}
-    >
-      COLLAPSE LINK
-    </MDBBtn>
-    <MDBBtn
-      color="primary"
-      onClick={this.toggleCollapse("basicCollapse")}
-      style={{ marginBottom: "1rem" }}
-    >
-      COLLAPSE BUTTON
-
-    </MDBBtn>
-        <MDBCollapse id="basicCollapse" isOpen={this.state.collapseID}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
-        </MDBCollapse>
         </>
         );
     }
